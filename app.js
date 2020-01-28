b20242ad9911e19585b4cd3273f54f442bf80e67
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-01-27 20:09:31 -0700

    recipes added

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ let recipe = {
     dicedTomatoes: 1,
     tomatoSauce: 1,
     onions: .5,
-    chickenBreast: 3
+    chickenBreast: 3,
+    tortillaChips: 1,
   },
   chili: {
     groundBeef: 1,
@@ -23,46 +24,103 @@ let recipe = {
     garlic: 1
   },
   potRoast: {
+    potRoastBeef: 1.5,
+    carrots: 1,
+    potatoes: 1,
+    onions: 1,
+    stock: 1,
+    garlic: 1
+  },
+  beanAndBaconSoup: {
+    bacon: 6,
+    carrots: .5,
+    onions: .5,
+    garlic: 1,
+    thyme: 1,
+    rosemary: 1,
+    stock: 1,
+    navyBeans: 1
+  },
+  stew: {
     stewBeef: 1.5,
     carrots: 1,
     potatoes: 1,
     onions: 1,
     stock: 1,
     garlic: 1
-
-  },
-  beanAndBaconSoup: {
-
-  },
-  stew: {
-
   },
   spaghetti: {
-
+    groundBeef: 1,
+    tomatoSauce: 3,
+    dicedTomatoes: 1,
+    tomatoPaste: 1,
+    basil: 1,
+    oregano: 1,
+    garlic: 1,
+    sugar: 1,
+    parsley: 1,
+    noodles: 1,
   },
   asianChickenWrap: {
-
+    groundChicken: 2,
+    garlic: 1,
+    bellPepper: 1,
+    onions: .5,
+    hoisinSauce: .5,
+    soySauce: 2,
+    waterChestnuts: 1, //8oz can
+    rice: 1.5,
+    romaineLettuce: 3,
+    riceVinegar: 1, //TBSP
+    sesameOil: 1.5 //tsp
   },
   chickenTeriyaki: {
-
+    chickenBreast: 3,
+    onions: .5,
+    crushedPinneapple: 1,
+    garlic: 1,
+    teriyakiSauce: .5
   },
   beefStroganoff: {
-
+    stewMeat: 2,
+    creamMushroomSoup: 2,
+    onions: .5,
+    worcestershireSauce: 2,
+    water: .5, //cups
+    sourCream: 1,
+    garlic: 1,
+    onionSoupPacket: 1,
+    choppedMushrooms: 1,
+    eggNoodles: 1
   },
   chickenNoodleSoup: {
-
+    chickenBreast: 3,
+    onions: 1,
+    carrots: 1,
+    celery: .5,
   },
   lemonPepperChicken: {
-
+    oliveOil: .25,
+    lemonJuice: 1, //from 1 lemon
+    pepper: 1, //tsp
   },
   balsalmicBaconChicken: {
-
+    chickenBreast: 3,
+    balsamicVinegar: .25,
+    stock: .5, //cup
+    garlic: 1,
+    sweetPotatoes: 1,
+    kale: 1, //bundle
+    carrots: .5,
+    bacon: 4
   },
   chickenTacos: {
-
+    chickenBreast: 3,
+    tacoSeasoning: 1
   },
   beefTacos: {
-
+    potRoastBeef: 1,
+    carneAsadaSeasoning: 1
   },
 
 }
