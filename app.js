546f935a6275ee6500a3c0361916c6f41ffac261
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-01-27 19:34:49 -0700

    added recipe names

---
+++ app.js
@@ -0,0 +1,70 @@
+let recipe = {
+  chickenFajitaSoup: {
+    garlic: 1,
+    blackBean: 1,
+    pintoBean: 1,
+    kidneyBean: 1,
+    frozenCorn: 1,
+    tacoSeasoning: 1,
+    dicedTomatoes: 1,
+    tomatoSauce: 1,
+    onions: .5,
+    chickenBreast: 3
+  },
+  chili: {
+    groundBeef: 1,
+    onions: .5,
+    tacoSeasoning: 1,
+    frozenCorn: 1,
+    blackBean: 1,
+    dicedTomatoes: 2,
+    tomatoSauce: 1,
+    dicedChilis: 1,
+    garlic: 1
+  },
+  potRoast: {
+    stewBeef: 1.5,
+    carrots: 1,
+    potatoes: 1,
+    onions: 1,
+    stock: 1,
+    garlic: 1
+
+  },
+  beanAndBaconSoup: {
+
+  },
+  stew: {
+
+  },
+  spaghetti: {
+
+  },
+  asianChickenWrap: {
+
+  },
+  chickenTeriyaki: {
+
+  },
+  beefStroganoff: {
+
+  },
+  chickenNoodleSoup: {
+
+  },
+  lemonPepperChicken: {
+
+  },
+  balsalmicBaconChicken: {
+
+  },
+  chickenTacos: {
+
+  },
+  beefTacos: {
+
+  },
+
+}
+
+// garlic : (#) "TBSP"
